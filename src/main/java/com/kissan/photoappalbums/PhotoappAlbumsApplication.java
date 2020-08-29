122dfad8597e4e64ce963771a00315cf53a14421
Kissan Naik <kissannaik@yahoo.com>
2020-08-29 11:06:29 +0530

    Album implementation

---
+++ src/main/java/com/kissan/photoappalbums/PhotoappAlbumsApplication.java
@@ -0,0 +1,13 @@
+package com.kissan.photoappalbums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoappAlbumsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoappAlbumsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kissan/photoappalbums/PhotoappAlbumsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kissan.photoappalbums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappAlbumsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
